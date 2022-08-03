304ca1a3dee4b458088169debded1577816d2644
agile-programmer <xusanovaxusninigor@gmail.com>
2022-08-03 20:28:25 +0500

    initial coment

--- src/App.js
+++ src/App.js
@@ -3,41 +3,41 @@ import React, { Component } from 'react'
 import './components/style.scss'
 
 class App extends Component {
-    state = { 
+    state = {
         value: 'click',
-     }
+    }
 
-     handleClick = (x) => {
-         if(x === 1){
-             this.setState({value:"toxtang"})
-         }
-         if(x === 2){
-             this.setState({value:"kuting"})
-         }
-         if(x === 3){
-             this.setState({value:"yuring"})
-         }
-     }
+    handleClick = (x) => {
+        if (x === 1) {
+            this.setState({ value: "toxtang" })
+        }
+        if (x === 2) {
+            this.setState({ value: "kuting" })
+        }
+        if (x === 3) {
+            this.setState({ value: "yuring" })
+        }
+    }
 
 
-    render() { 
+    render() {
         return (
-           <>
-           <div className='svetafor'>
-               <div className="card1 card" onClick={() =>this.handleClick(1)}>
-                   <h1>qizil</h1>
-               </div>
-               <div className="card2 card" onClick={() =>this.handleClick(2)}>
-                   <h1>sariq</h1>
-               </div>
-               <div className="card3 card" onClick={() =>this.handleClick(3)}>
-                   <h1>yashil</h1>
-               </div>
-           </div>
-           <h1>{this.state.value}</h1>
-           </>
+            <>
+                <div className='svetafor'>
+                    <div className="card1 card" onClick={() => this.handleClick(1)}>
+                        <h1>qizil</h1>
+                    </div>
+                    <div className="card2 card" onClick={() => this.handleClick(2)}>
+                        <h1>sariq</h1>
+                    </div>
+                    <div className="card3 card" onClick={() => this.handleClick(3)}>
+                        <h1>yashil</h1>
+                    </div>
+                </div>
+                <h1>{this.state.value}</h1>
+            </>
         );
     }
 }
- 
+
 export default App;
